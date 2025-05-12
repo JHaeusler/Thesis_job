19234926f672b0bd01005557062cc07069942e3c
JHaeusler <juan.haeusler@correounivalle.edu.co>
2025-05-11 22:06:25 -0500

    avance 11-5-2025

--- bayesian_analysis.R
+++ bayesian_analysis.R
@@ -1,3 +1,16 @@
-#--- Hello World ---
+#--- Bayesian Inference ----
 
-"Hello world"
+setwd("D:/Github/Thesis_job")
+
+lista_de_paquetes <- c("readxl", "ggplot2", "readr", "tidyr") # Reemplaza con tus paquetes
+
+for (paquete in lista_de_paquetes) {
+  if(!require(paquete, character.only = TRUE)){
+    install.packages(paquete)
+    require(paquete, character.only = TRUE)
+  }
+}
+
+data1 <- read_excel("Acceptance Sampling MIL-STD 105E for Quality Control.xlsx",
+                    sheet = "tab")
+data2 <- read_excel("Jurnal Rekavasi.xlsx", sheet = "tabla")
