494605464abbfa38610e99f651c5b4929afe09ef
JHaeusler <juan.haeusler@correounivalle.edu.co>
2025-05-13 17:00:15 -0500

    # inicio replica codigo Jerzy

--- bayesian_analysis.R
+++ bayesian_analysis.R
@@ -15,8 +15,47 @@ data1 <- read_excel("Acceptance Sampling MIL-STD 105E for Quality Control.xlsx",
                     sheet = "tab")
 data2 <- read_excel("Jurnal Rekavasi.xlsx", sheet = "tabla")
 
+#---- Metropolis-Hastings ----
 
+x0 <- c(0.5); burn_in <- 5000; n_samples <- 1000; thinning <- 1
 
+samples = NULL
+x_currente <- array(data = NA, dim = c(1, length(x0)), dimnames = NULL)
 
+total_iterations = burn_in + n_samples * thinning 
+accepted_samples <- NULL
+
+i <- 0
+
+while(i < total_iterations){
+  # prior propuesta
+  x_proposed <- proposal_sampler(x_current)
+  # Compute denominator of the acceptance ratio
+  denominator <- target_pdf(x_current) * proposal_pdf(x_proposed, x_current)
+  
+  # Check if the denominator is nonzero
+  if (denominator > 0){
+    acceptance_ratio = (target_pdf(x_proposed) * proposal_pdf(x_current, x_proposed)) / denominator
+  acceptance_ratio = min(1, acceptance_ratio)
+  } else{
+  # Accept or reject the proposal
+  if (runif(0, 1) < acceptance_ratio){
+    x_current = x_proposed  # Accept proposal
+  accepted_samples <- accepted_samples + 1  # Increment acceptance counter
+  
+  # Store sample only after the burn-in period and at every 'thinning' interval
+  } else{
+  if (i >= burn_in && (i - burn_in) %% thinning == 0){
+    samples.append(x_current)
+  }
+  }
+  # Only increment i if the calculation was valid
+  i <- i + 1  # Advance iteration
+}  
+  # Compute acceptance rate
+  acceptance_rate = accepted_samples / total_iterations
+  
+  return(samples, acceptance_rate)
+}
 
 
